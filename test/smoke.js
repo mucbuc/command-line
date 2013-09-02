74200bd90d14805b170d29984a4b7e7f469d555a
markymark <smallitle@hotmail.com>
2013-09-02 14:55:14 -0700

    added text area

--- test/smoke.js
+++ test/smoke.js
@@ -1,48 +1,60 @@
 window.addEventListener( 'load', function() {
 
-	var element = document.getElementById( 'commandLine' )
-    , emitter = new EventStream()
- 	  , cl = new CommandLine( element, emitter )
-    , TIMEOUT = 0;
+	var emitter = new EventStream()
+    , TIMEOUT = 0
+    , line = document.getElementById( 'commandLine' )
+    , area = document.getElementById( 'commandArea' );
 
-  cl.on( 'Tab', checkActive );
-  cl.on( 'Up', checkCarret );
-  cl.on( 'Down', checkCarret );
-  cl.on( 'Enter', emitter.tick );
-
+  // need to test these 
   emitter.on( 'auto', function() { console.log( 'auto' ); } );
   emitter.on( 'eval', function() { console.log( 'eval' ); } );
   emitter.on( 'previous', function(e) { console.log( 'previous' ); } );
   emitter.on( 'next', function() { console.log( 'next' ); } );
 
-  function checkActive( e ) {
-    if (  e.action == 'activate' 
-       || e.action == 'repeat') {
-      setTimeout( function() {
-        if(element.value && document.activeElement != element) {
-          failTest();
-        }
-        emitter.tick();
-      }, TIMEOUT ); 
+  testElement( line ); 
+  testElement( area ); 
+
+
+  function testElement( element ) {
+
+    var cl = new CommandLine( element, emitter )
+    
+    cl.on( 'Tab', checkActive );
+    cl.on( 'Up', checkCarret );
+    cl.on( 'Down', checkCarret );
+    cl.on( 'Enter', emitter.tick );
+
+    function checkActive( e ) {
+      if (  e.action == 'activate' 
+         || e.action == 'repeat') {
+        setTimeout( function() {
+          if(element.value && document.activeElement != element) {
+            failTest();
+          }
+          emitter.tick();
+        }, TIMEOUT ); 
+      }
+    }
+
+    function checkCarret(e) {
+      var start = element.selectionStart
+        , end = element.selectionEnd;
+      if (  e.action == 'activate' 
+         || e.action == 'repeat') {
+        var start = element.selectionStart
+          , end = element.selectionEnd;
+        setTimeout( function() {
+          if (  start != element.selectionStart
+             || end != element.selectionEnd) {
+            failTest();
+          }
+          emitter.tick();
+        }, TIMEOUT );
+      }
     }
   }
 
-  function checkCarret(e) {
-    var start = element.selectionStart
-      , end = element.selectionEnd;
-    if (  e.action == 'activate' 
-       || e.action == 'repeat') {
-      var start = element.selectionStart
-        , end = element.selectionEnd;
-      setTimeout( function() {
-        if (  start != element.selectionStart
-           || end != element.selectionEnd) {
-          failTest();
-        }
-        emitter.tick();
-      }, TIMEOUT );
-    }
-  }
+
 
   function failTest() {
     window.alert( 'test failed' );
