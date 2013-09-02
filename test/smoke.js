47aae7c05a54d50cb85b6a910b96c22699e90df5
markymark <smallitle@hotmail.com>
2013-09-01 20:01:05 -0700

    smoke test

--- test/smoke.js
+++ test/smoke.js
@@ -1,3 +1,17 @@
-(function(){
-	console.log( 'ran smoke.js' );
-})();
+window.addEventListener( 'load', function() { 
+
+	var element = document.getElementById( 'commandLine' )
+  	  , cl = new CommandLine( element );
+
+  	cl.on( 'Tab', function( e ) {
+  		window.setTimeout( function() {
+			if(		element.value
+				&&	document.activeElement != element) {
+  	 			window.alert( 'test failed' );
+  	 		}
+  	 	}, 10 );
+  	 	console.log( 'Tab up' );
+  	} );
+} );
+
+
